b0c91c7d9bfa3301ecd33e06de549193cbd25007
icarus <448654909@qq.com>
2018-12-29 17:59:52 +0800

    fix：修正了xixinyun打包配置api地址错误的问题

--- config/yun.env.js
+++ config/yun.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_PATH: '"http://beta-api.xixincloud.com/api/User"',
+  API_PATH: '"http://api.xixinyun.com/api/User"',
   // API_PATH: '"http://beta-api.xixincloud.com/api/User"',
   IMG_PATH: '"http://image.xixincloud.com"'
 }
