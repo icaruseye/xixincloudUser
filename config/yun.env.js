294b023141ed25c7f3e7c533f5f9d225d88a7a9d
icarus <448654909@qq.com>
2018-12-29 20:02:10 +0800

    fix：更正yun打包配置图片服务器地址

--- config/yun.env.js
+++ config/yun.env.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: '"production"',
   API_PATH: '"http://api.xixinyun.com/api/User"',
   // API_PATH: '"http://beta-api.xixincloud.com/api/User"',
-  IMG_PATH: '"http://image.xixincloud.com"'
+  IMG_PATH: '"http://image.xixinyun.com"'
 }
